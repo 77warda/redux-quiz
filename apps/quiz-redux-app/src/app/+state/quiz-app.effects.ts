c75982989f9a440091d14bb34d3b87ec6cfda987
Warda Rasool <89411637+77warda@users.noreply.github.com>
2024-02-12 18:41:33 +0500

    set loadQuestionsSuccess as the ofType action in timer start

--- apps/quiz-redux-app/src/app/+state/quiz-app.effects.ts
+++ apps/quiz-redux-app/src/app/+state/quiz-app.effects.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { createEffect, Actions, ofType } from '@ngrx/effects';
+import { createEffect, Actions, ofType, concatLatestFrom } from '@ngrx/effects';
 import {
   catchError,
   of,
@@ -28,49 +28,56 @@ export class QuizAppEffects {
     private store: Store
   ) {}
 
+  startTimer$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(QuizApiActions.loadQuestionsSuccess),
+        concatLatestFrom(() => this.store.select(selectTotalQuestions)),
+        switchMap(([action, totalQuestions]) => {
+          const timerDuration = totalQuestions * 10;
+          console.log('Timer started');
+          return interval(1000).pipe(
+            takeUntil(this.actions$.pipe(ofType(QuizActions.finishQuiz))),
+            map((timeElapsed) => timerDuration - timeElapsed),
+            tap((remainingTime) => {
+              if (remainingTime === 0) {
+                this.store.dispatch(QuizActions.finishQuiz());
+              }
+              const minutes = Math.floor(remainingTime / 60);
+              const seconds = remainingTime % 60;
+              const formattedMinutes =
+                minutes < 10 ? '0' + minutes : '' + minutes;
+              const formattedSeconds =
+                seconds < 10 ? '0' + seconds : '' + seconds;
+              console.log(
+                'Time remaining:',
+                `${formattedMinutes}:${formattedSeconds}`
+              );
+              this.store.dispatch(
+                QuizActions.updateTimer({
+                  timer: `${formattedMinutes}:${formattedSeconds}`,
+                })
+              );
+            })
+          );
+        })
+      ),
+    { dispatch: false }
+  );
+
   loadTrivia$ = createEffect(() =>
     this.actions$.pipe(
       ofType(QuizActions.submitForm),
-      mergeMap((action) =>
+      switchMap((action) =>
         this.quizService.getTrivia(action.formValue).pipe(
           tap(() => {
             console.log('Form submitted successfully');
             this.router.navigate(['/quizstart']);
           }),
-          switchMap((quizQuestions) => {
-            const totalQuestions = action.formValue.numberOfQuestions;
-            const timerDuration = totalQuestions * 10;
-
-            console.log('Timer started');
-            interval(1000)
-              .pipe(
-                take(timerDuration + 1),
-                tap((timeElapsed) => {
-                  const remainingTime = timerDuration - timeElapsed;
-                  const minutes = Math.floor(remainingTime / 60);
-                  const seconds = remainingTime % 60;
-                  const formattedMinutes =
-                    minutes < 10 ? '0' + minutes : '' + minutes;
-                  const formattedSeconds =
-                    seconds < 10 ? '0' + seconds : '' + seconds;
-                  // console.log(
-                  //   'Time remaining:',
-                  //   `${formattedMinutes}:${formattedSeconds}`
-                  // );
-                  this.store.dispatch(
-                    QuizActions.updateTimer({
-                      timer: `${formattedMinutes}:${formattedSeconds}`,
-                    })
-                  );
-                  if (remainingTime === 0) {
-                    this.store.dispatch(QuizActions.finishQuiz());
-                  }
-                })
-              )
-              .subscribe();
-
-            return [QuizApiActions.loadQuestionsSuccess({ quizQuestions })];
-          }),
+          switchMap((quizQuestions) => [
+            QuizApiActions.loadQuestionsSuccess({ quizQuestions }),
+            QuizActions.startTimer(),
+          ]),
           catchError((error) => {
             console.error('Error in loadTrivia effect:', error);
             return of(QuizApiActions.loadQuestionsFailure({ error }));
